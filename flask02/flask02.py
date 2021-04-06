faff425c93cb6c27b3c7ed53220c649172946eff
Jenna Bath <jbath@uncc.edu>
2021-04-06 13:12:22 -0400

    flask app #2 - created initial app file

--- flask02/flask02.py
+++ flask02/flask02.py
@@ -1,23 +1,26 @@
 # FLASK Tutorial 1 -- We show the bare bones code to get an app up and running
 
 # imports
-import os                 # os is used to get environment variables IP & PORT
-from flask import Flask, render_template   # Flask is the web app that we will customize
+import os  # os is used to get environment variables IP & PORT
+from flask import Flask, render_template  # Flask is the web app that we will customize
 
-app = Flask(__name__)     # create an app
+app = Flask(__name__)  # create an app
+
 
 # @app.route is a decorator. It gives the function "index" special powers.
 # In this case it makes it so anyone going to "your-url/" makes this function
 # get called. What it returns is what is shown as the web page
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    a_user = {'name': 'Jenna', 'email': 'jbath@uncc.edu'}
+
+    return render_template("index.html", user=a_user)
 
 
-app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
+app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 5000)), debug=True)
 
 # To see the web page in your web browser, go to the url,
-#   http://127.0.0.1:5000
+#   http://127.0.0.1:5000/index
 
 # Note that we are running with "debug=True", so if you make changes and save it
 # the server will automatically update. This is great for development but is a
